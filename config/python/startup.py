3ceb3cd0093287cef15e6dc3b8371286f7b09719
Loïc Reynier <loic@loicreynier.fr>
2024-01-19 12:12:46 +0000

    refactor(config.python): add version compatibility

--- config/python/startup.py
+++ config/python/startup.py
@@ -1,32 +1,59 @@
-# TODO: add documentation
-
-import atexit
-import readline
 import importlib
 import os
+import sys
 
 
-def execfile(path: str) -> None:
+# -- Utility functions
+def execfile(path):
     """Execute file from `path`."""
     exec(open(path, encoding="utf-8").read())
 
 
-def _save_history(prev_history_len: int, file_: str) -> None:
+# -- Save history to `$XDG_STATE_HOME/python_history` for Python < 3.13
+# Python 3.13 has this functionnaliy built in with `$PYTHON_HISTORY`.
+def _save_history(prev_history_len, file_):
     """Save console history in `file_`."""
     new_history_len = readline.get_current_history_length()
     readline.set_history_length(10000)
     readline.append_history_file(new_history_len - prev_history_len, file_)
 
 
-_history_file = os.environ.get("XDG_STATE_HOME", "@state@") + "/python_history"
+_version = sys.version_info
+if _version < (3, 13):
+    try:
+        import atexit
+        import readline
 
-try:
-    readline.read_history_file(_history_file)
-    _history_len = readline.get_current_history_length()
-except FileNotFoundError:
-    open(_history_file, "wb").close()
-    _history_len = 0
+        # -- Python 2
+        if _version < (3,):
+            _history_file = (
+                os.environ.get("XDG_STATE_HOME", "@state@")
+                + "/python2_history"
+            )
+            try:
+                readline.read_history_file(_history_file)
+            except IOError:
+                pass
+            atexit.register(readline.write_history_file, _history_file)
 
+        # -- Python 3 < 3.13
+        else:
+            _history_file = (
+                os.environ.get("XDG_STATE_HOME", "@state@")
+                + "/python_history"
+            )
+            try:
+                readline.read_history_file(_history_file)
+                _history_len = readline.get_current_history_length()
+            except FileNotFoundError:
+                open(_history_file, "wb").close()
+                _history_len = 0
+            atexit.register(_save_history, _history_len, _history_file)
+
+    except ImportError:
+        pass
+
+# -- Auto import modules set in `$PYTHONMODULES`
 _module = ""
 _alias = ""
 for _module in os.environ.get("PYTHONMODULES", "").split(":"):
@@ -34,6 +61,4 @@ for _module in os.environ.get("PYTHONMODULES", "").split(":"):
         _alias = _module.split("=")[1] if "=" in _module else _module
         locals()[_alias] = importlib.import_module(_module.split("=")[0])
 
-del _module, _alias
-
-atexit.register(_save_history, _history_len, _history_file)
+del _module, _alias, _version
