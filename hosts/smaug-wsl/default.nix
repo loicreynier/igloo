2b638a58174cbc9577ddb3ca3a488cec812653e8
Loïc Reynier <loic@loicreynier.fr>
2024-03-28 17:15:39 +0000

    feat(hosts): add SSH servers

--- hosts/smaug-wsl/default.nix
+++ hosts/smaug-wsl/default.nix
@@ -1,9 +1,11 @@
 {
   system.stateVersion = "23.11";
 
-  networking.hostName = "smaug";
+  networking.hostName = "smaug-wsl";
 
   igloo.device.type = "wsl";
+
+  services.openssh.ports = [2201];
 
   wsl.defaultUser = "loic";
 }
