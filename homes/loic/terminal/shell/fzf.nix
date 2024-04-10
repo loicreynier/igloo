26eef81342d4596ee44cc8ba8499fb600518e353
Loïc Reynier <loic@loicreynier.fr>
2024-04-10 14:32:07 +0000

    feat(loic.fzf): add Just chooser

--- homes/loic/terminal/shell/fzf.nix
+++ homes/loic/terminal/shell/fzf.nix
@@ -2,16 +2,32 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  fzfBin = "${config.programs.fzf.package}/bin/fzf";
+  batBin = "${pkgs.bat}/bin/bat";
+  justBin = "${pkgs.just}/bin/just";
+in {
   programs.fzf = {
     enable = true;
     defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden --follow --strip-cwd-prefix";
   };
 
-  home.shellAliases = let
-    fzfBin = "${config.programs.fzf.package}/bin/fzf";
-    batBin = "${pkgs.bat}/bin/bat";
-  in {
-    v = "${fzfBin} --multi --bind 'enter:become(vi {+})' --preview '${batBin} --color=always {}' --height 40% --layout reverse";
+  home = {
+    shellAliases = {
+      v = "${fzfBin} --multi --bind 'enter:become(vi {+})' --preview '${batBin} --color=always {}' --height 40% --layout reverse";
+    };
+    sessionVariables = {
+      JUST_CHOOSER = builtins.concatStringsSep " " [
+        fzfBin
+        "--border=top"
+        "--border-label='Parameterless Just recipes'"
+        "--height=40%"
+        "--margin='5%,0%,5%,0%'"
+        "--preview-window=right:85%:wrap:nocycle"
+        "--preview='${justBin} --show {} | ${batBin} --language=Just --color=always --style=plain'"
+        "--bind=ctrl-u:preview-page-up,ctrl-d:preview-page-down"
+        "|| false"
+      ];
+    };
   };
 }
