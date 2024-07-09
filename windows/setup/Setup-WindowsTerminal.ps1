54f2aee547905ae1d12f9a7be5d7b671b98709fe
Loïc Reynier <loic@loicreynier.fr>
2024-07-09 13:48:11 +0000

    feat(windows.setup): add Windows Terminal settings

--- windows/setup/Setup-WindowsTerminal.ps1
+++ windows/setup/Setup-WindowsTerminal.ps1
@@ -27,7 +27,7 @@ function Update-WindowsTerminal {
         [switch]$Preview
     )
 
-    Write-Host "Updating Windows Terminal..."
+    Write-Host -ForegroundColor DarkCyan "Updating Windows Terminal..."
 
     winget install --id Microsoft.WindowsTerminal --source winget
 
@@ -36,12 +36,55 @@ function Update-WindowsTerminal {
     }
 }
 
+# editorconfig-checker-disable
+function Update-WindowsTerminalSettings {
+    param (
+        [string]$SourceURL = "https://github.com/loicreynier/igloo/raw/dev/config/WindowsTerminal/settings.json",
+        [switch]$Preview
+    )
+
+    Write-Host -ForegroundColor DarkCyan "Updating/Installing Windows Terminal settings file..."
+
+    if ($Preview) {
+        $settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+    }
+    else {
+        # $settingsPath = "$env:LOCALAPPDATA\Microsoft\Windows Terminal\settings.json"
+        $settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+    }
+
+    if (!(Test-Path -Path $settingsPath -PathType Leaf)) {
+        try {
+            New-Item -Path ([System.IO.Path]::GetDirectoryName($settingsPath)) -ItemType Directory -Force
+            Invoke-RestMethod $SourceURL -OutFile $settingsPath
+            Write-Host "Windows Terminal settings file created at '$settingsPath'"
+        }
+        catch {
+            Write-Error "Failed to create settings file. Error: $_"
+        }
+    }
+    else {
+        try {
+            Write-Host "Found existing settings file"
+            Move-Item -Path $settingsPath -Destination "$settingsPath.old" -Force
+            Invoke-RestMethod $SourceURL -OutFile $settingsPath
+            Write-Host "Settings file updated. Old profile backed-up at '$settingsPath.old'"
+        }
+        catch {
+            Write-Error "Failed to backup and update settings file. Error: $_"
+        }
+    }
+}
+# editorconfig-checker-enable
+
 function Install-NerdFonts {
     param (
         [string]$FontName = "CascadiaCode",
         [string]$FontDisplayName = "CaskaydiaCove NF",
         [string]$Version = "3.2.1"
     )
+
+    Write-Host -ForegroundColor DarkCyan "Installing Nerd Font ${FontDisplayName} (${FontName})..."
 
     try {
         [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
@@ -70,7 +113,8 @@ function Install-NerdFonts {
 
             Remove-Item -Path $extractPath -Recurse -Force
             Remove-Item -Path $zipFilePath -Force
-        } else {
+        }
+        else {
             Write-Host "Font ${FontDisplayName} already installed"
         }
     }
@@ -81,6 +125,7 @@ function Install-NerdFonts {
 
 if (Test-InternetConnection) {
     Update-WindowsTerminal -Preview
+    Update-WindowsTerminalSettings # -Preview
     Install-NerdFonts -FontName "CascadiaCode" -FontDisplayName "CaskaydiaCove NF"
 }
 else {
