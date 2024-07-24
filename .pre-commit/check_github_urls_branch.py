3e7e5f31b0880f0a02f15980710142cca429ff84
Loïc Reynier <loic@loicreynier.fr>
2024-07-24 16:52:42 +0000

    build(pre-commit): fix GitHub URLs branch check regex

--- .pre-commit/check_github_urls_branch.py
+++ .pre-commit/check_github_urls_branch.py
@@ -1,8 +1,10 @@
 import re
+import subprocess
 import sys
 
 USER = "loicreynier"
 REPO = "igloo"
+BRANCH = "main"
 
 
 def check_github_urls(file_paths, user, repo, branch="main"):
@@ -10,7 +12,7 @@ def check_github_urls(file_paths, user, repo, branch="main"):
     url_pattern = re.compile(
         r"https://(?:raw\.githubusercontent\.com|github\.com)/"
         f"{user}/{repo}/"
-        r"raw/([^/]+)/"
+        r"([^/]+)/"
     )
     all_good = True
 
@@ -19,10 +21,10 @@ def check_github_urls(file_paths, user, repo, branch="main"):
             lines = f.readlines()
             for line_num, line in enumerate(lines, 1):
                 match = url_pattern.search(line)
-                if match and match.group(1) != "main":
+                if match and match.group(1) != branch:
                     print(
                         f"URL in file '{file_path}', line {line_num} "
-                        f"targets branch '{match.group(1)}' instead of 'main'.",
+                        f"targets branch '{match.group(1)}' instead of '{branch}'.",
                         file=sys.stderr,
                     )
                     all_good = False
@@ -30,13 +32,30 @@ def check_github_urls(file_paths, user, repo, branch="main"):
     return all_good
 
 
+def current_branch():
+    try:
+        result = subprocess.run(
+            ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Error checking current branch: {e}", file=sys.stderr)
+        return None
+
+
 def main():
+    if current_branch() != BRANCH:
+        sys.exit(0)
+
     file_paths = sys.argv[1:]
 
     if not file_paths:
         sys.exit(0)
 
-    if not check_github_urls(file_paths, USER, REPO):
+    if not check_github_urls(file_paths, USER, REPO, branch=BRANCH):
         sys.exit(1)
 
     sys.exit(0)
