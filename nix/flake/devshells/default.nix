d0036813f4e25b94df26b5aecca0ee180513bdbb
Loïc Reynier <loic@loicreynier.fr>
2024-09-11 08:13:03 +0000

    feat(devshell.default): add flake feature check

--- nix/flake/devshells/default.nix
+++ nix/flake/devshells/default.nix
@@ -11,6 +11,24 @@
 
       shellHook = ''
         ${config.pre-commit.installationScript}
+
+        check_flakes_enabled() {
+          # Easier to check directly on the command output rather than
+          # looking for `/etc/nix/nix.conf` or `~/.config/nix/nix.conf`
+          nix flake --help &>/dev/null
+
+          if [ $? -eq 0 ]; then
+            return 0
+          else
+            return 1
+          fi
+        }
+
+        if ! check_flakes_enabled; then
+          echo "Nix flakes are not enabled"
+          echo "You can enable them by running the following command:"
+          echo "echo 'experimental-features = nix-command flakes' >>~/.config/nix/nix.conf"
+        fi
       '';
 
       DIRENV_LOG_FORMAT = ""; # Force direnv to shut up
