43dfa1294cb9f1e5acfe97c86e0eba7949e2931e
Loïc Reynier <loic@loicreynier.fr>
2024-10-21 10:09:26 +0200

    refactor(unix.scripts): install `stowsh` as Stow package

--- unix/scripts/install-stowsh.bash
+++ unix/scripts/install-stowsh.bash
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-url="https://raw.githubusercontent.com/mikepqr/stowsh/refs/heads/main/stowsh"
-dest="$HOME/.local/bin/stowsh"
+repo="mikepqr/stowsh"
+file="stowsh"
 
-mkdir -p "$HOME/.local/bin"
-curl -L -o "$dest" "$url"
+read -r sha date <<<"$(curl -s "https://api.github.com/repos/$repo/commits" |
+  # Just showing off some AWK scripting
+  awk -F'"' '
+    /"sha"/ {sha=$4}
+    /"date"/ {date=$4; split(date, d, "T"); print sha, d[1]; exit}
+  ')"
+
+dest="$HOME/.local/stow/stowsh-git-$date/bin"
+mkdir -p "$dest"
+dest="$dest/$file"
+
+curl -s "https://raw.githubusercontent.com/$repo/$sha/$file" -o "$dest"
 chmod +x "$dest"
-
 echo "\`stowsh\` installed in $dest"
+
+ln -s "$dest" "$HOME/.local/bin" && echo "\`stowsh\` linked in $HOME/.local/bin"
 
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
   echo "Warning: \$HOME/.local/bin is not in your PATH."
