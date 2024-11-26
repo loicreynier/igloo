f8f1851214e2cba7b792ff2e52f93f3573838566
Loïc Reynier <loic@loicreynier.fr>
2024-11-26 16:43:37 +0100

    feat(config.neovim): add `smartcolumn` plugin

---
+++ config/neovim/lua/lazy/plugins/smartcolumn.lua
@@ -0,0 +1,24 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  event = "VeryLazy",
+  opts = {
+    disabled_filetypes = {
+      "help",
+      "checkhealth",
+      "lspinfo",
+      "lazy",
+      "alpha",
+      "noice",
+      "Trouble",
+      "neo-tree",
+      "NvimTree",
+    },
+    custom_colorcolumn = function()
+      local column = 80
+      if vim.b[0].editorconfig and vim.b[0].editorconfig.max_line_length then
+        column = vim.b[0].editorconfig.max_line_length
+      end
+      return column
+    end,
+  },
+}
