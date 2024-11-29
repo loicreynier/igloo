50f272dc83a6d4b4374841fe8f8ad9249b220599
Loïc Reynier <loic@loicreynier.fr>
2024-11-29 14:55:21 +0100

    feat(config.neovim): add `guess-indent`

---
+++ config/neovim/lua/lazy/plugins/guess-indent.lua
@@ -0,0 +1,14 @@
+local utils = require("utils")
+
+return {
+  "NMAC427/guess-indent.nvim",
+  cmd = "GuessIndent",
+  opts = {
+    auto_cmd = true,
+    buftype_exclude = utils.disabled_buftypes,
+    filetype_exclude = utils.disabled_filetypes,
+  },
+  config = function(_, opts)
+    require("guess-indent").setup(opts)
+  end,
+}
