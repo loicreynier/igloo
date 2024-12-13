c8491915523c0d68b9f41fa3f107556af0a0ffcb
Loïc Reynier <loic@loicreynier.fr>
2024-12-13 14:59:48 +0100

    feat(config.neovim): add Beacon plugin

---
+++ config/neovim/lua/plugins/lazy/beacon.lua
@@ -0,0 +1,8 @@
+return {
+  "DanilaMihailov/beacon.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    cursor_events = { "CursorMoved" },
+    window_events = { "WinEnter", "FocusGained"},
+  },
+}
