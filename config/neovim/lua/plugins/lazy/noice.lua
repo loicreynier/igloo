9f262c4b64956c169b052d5f67b7526f7c423f95
Loïc Reynier <loic@loicreynier.fr>
2024-12-16 22:40:05 +0100

    refactor(config.neovim): remove version lock on Noice

--- config/neovim/lua/plugins/lazy/noice.lua
+++ config/neovim/lua/plugins/lazy/noice.lua
@@ -6,7 +6,6 @@
 
 return {
   "folke/noice.nvim",
-  version = "<=4.4.7", -- FIX: https://github.com/folke/noice.nvim/issues/938
   event = "VeryLazy",
   override = {
     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
