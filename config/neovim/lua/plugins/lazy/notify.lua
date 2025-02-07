35f9005dd1b095d76364a95a93f3d35d5c35ed9e
Loïc Reynier <loic@loicreynier.fr>
2025-02-07 20:35:16 +0100

    fix(config.neovim): fix `notify`

--- config/neovim/lua/plugins/lazy/notify.lua
+++ config/neovim/lua/plugins/lazy/notify.lua
@@ -7,10 +7,10 @@ return {
       desc = "Dismiss all notifications (Notify)",
     },
   },
-  config = function() vim.notify = require("notify") end,
   opts = {
     stages = "static",
     timeout = 3000,
+    background_colour = "#000000",
     max_height = function() return math.floor(vim.o.lines * 0.75) end,
     max_width = function() return math.floor(vim.o.columns * 0.75) end,
     on_open = function(win) vim.api.nvim_win_set_config(win, { zindex = 100 }) end,
