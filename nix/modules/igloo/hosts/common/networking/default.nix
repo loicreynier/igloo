18c2d73f8eb854255b1e0252a12cf5a1d59806e2
Loïc Reynier <loic@loicreynier.fr>
2025-06-04 19:58:44 +0200

    refactor(hosts.netw): disable NordVPN on other device types

--- nix/modules/igloo/hosts/common/networking/default.nix
+++ nix/modules/igloo/hosts/common/networking/default.nix
@@ -1,6 +1,18 @@
 {
+  lib,
+  config,
+  ...
+}:
+let
+  useVPN = lib.any (device: device == config.igloo.device.type) [
+    "desktop"
+    "laptop"
+    "workstation"
+  ];
+in
+{
   # NOTE: User must be added to the `nordvpn` group (`users.users.extraGroups`)
   # and port UDP 1194 and TCP 443 must be allowed through the firewall
-  chaotic.nordvpn.enable = true;
-  networking.firewall.checkReversePath = false;
+  chaotic.nordvpn.enable = useVPN;
+  networking.firewall.checkReversePath = !useVPN;
 }
