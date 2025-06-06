0272f9a55a91030c5cb671de337fb87f4068c68e
Loïc Reynier <loic@loicreynier.fr>
2025-06-06 15:42:42 +0200

    feat(config.neovim): add Hardtime

---
+++ config/neovim/lua/plugins/lazy/hardtime.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    disabled_filetypes = require("utils").disabled_filetypes,
+    disable_mouse = false,
+  },
+}
