42a41db6457619e1953203dc2d561fe535d75f03
Loïc Reynier <loic@loicreynier.fr>
2025-07-13 23:38:29 +0200

    feat(loic.desktop): add apps & GTK theming

--- nix/homes/loic/desktop.nix
+++ nix/homes/loic/desktop.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
+let
+  cursorTheme = {
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    size = 20;
+  };
+in
 {
   programs = {
     firefox = {
@@ -18,9 +25,38 @@
     };
   };
 
-  home.packages = with pkgs; [
-    discord
-    spotify-adblock
-    thunderbird # Don't configure profiles with HM, yet
-  ];
+  home = {
+    packages = with pkgs; [
+      discord
+      spotify-adblock
+      thunderbird
+
+      libreoffice
+      meld
+      pdfarranger
+      picard
+      pympress
+    ];
+
+    # Disable `adw-gtk3` for GTK 4 apps, see `gtk.theme.name` and `xdg.configFile."gtk-4.0/gtk.css"`
+    file.".config/gtk-4.0/gtk.css".text = '''';
+
+    pointerCursor = cursorTheme;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+    };
+    iconTheme = {
+      name = "MoreWaita";
+      package = pkgs.morewaita-icon-theme;
+    };
+    inherit cursorTheme;
+  };
+
+  # Disable `adw-gtk3` for GTK 4 apps
+  xdg.configFile."gtk-4.0/gtk.css".enable = false;
 }
