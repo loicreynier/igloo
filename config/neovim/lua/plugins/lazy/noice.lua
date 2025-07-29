9ecdc8124ba7b64a934c7e10418014318649deeb
Loïc Reynier <loic@loicreynier.fr>
2025-07-29 16:44:05 +0200

    refactor(config.nvim): change Noice presets + add Telescope extension

--- config/neovim/lua/plugins/lazy/noice.lua
+++ config/neovim/lua/plugins/lazy/noice.lua
@@ -4,17 +4,36 @@
 
 --]]
 
+---@type LazySpec
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  override = {
-    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-    ["vim.lsp.util.stylize_markdown"] = true,
-    ["cmp.entry.get_documentation"] = true,
-  },
-  opts = {},
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
+  keys = {
+    { "<Leader>sn", "<Cmd>Noice telescope<CR>", desc = "Search Noice notifications (Telescope)" },
+  },
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    ---@type NoicePresets
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    require("noice").setup(opts)
+    telescope.load_extension("noice")
+  end,
 }
