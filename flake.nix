176563df9521b63ae68993c201105f5c234526c9
Loïc Reynier <loic@loicreynier.fr>
2025-11-14 14:23:53 +0100

    feat(nix): update flake inputs & add `no-shell`

--- flake.nix
+++ flake.nix
@@ -51,6 +51,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noshell = {
+      url = "github:viperML/noshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
--- nix/modules/igloo/hosts/common/env/default.nix
+++ nix/modules/igloo/hosts/common/env/default.nix
@@ -9,7 +9,13 @@
 {
   imports = [
     inputs.nix-index-database.nixosModules.nix-index
+    inputs.noshell.nixosModules.default
   ];
+
+  users.users.root.shell = pkgs.bashInteractive;
+  programs.noshell.enable = true;
+  # Allows HM users to configure their shell with
+  # xdg.configFile."shell".source = lib.getExe pkgs.nushell;
 
   environment = {
     defaultPackages = lib.mkForce [ ];
@@ -27,6 +33,8 @@
         wget
         zip
         unzip
+
+        kmon
       ]
       ++ lib.optionals (config.igloo.device.gpu.type == "nvidia") [
         nvitop
@@ -36,39 +44,36 @@
   programs = {
     less.enable = true;
     starship.enable = true;
-  };
-
-  programs = {
     command-not-found.enable = lib.mkForce false;
     nix-index-database.comma.enable = true;
-  };
 
-  programs.direnv = {
-    enable = true;
-    silent = true;
-    nix-direnv = {
+    direnv = {
       enable = true;
-      # In case alternative `nix` package is used, such as `nix-super`
-      package = pkgs.nix-direnv.override {
-        nix = config.nix.package;
+      silent = true;
+      nix-direnv = {
+        enable = true;
+        # In case alternative `nix` package is used, such as `nix-super`
+        package = pkgs.nix-direnv.override {
+          nix = config.nix.package;
+        };
       };
+      loadInNixShell = true;
+      direnvrcExtra = lib.fileContents (
+        pkgs.replaceVars "${self}/config/direnv/direnvrc.sh" { sha1sum = "${pkgs.perl}/bin/shasum"; }
+      );
     };
-    loadInNixShell = true;
-    direnvrcExtra = lib.fileContents (
-      pkgs.replaceVars "${self}/config/direnv/direnvrc.sh" { sha1sum = "${pkgs.perl}/bin/shasum"; }
-    );
+
+    nano =
+      let
+        nanoConf = lib.strings.fileContents "${self}/config/nano/nanorc";
+      in
+      {
+        enable = true;
+        nanorc = ''
+          include ${pkgs.nanorc}/share/*.nanorc # Extended syntax highlighting
+
+          ${nanoConf}
+        '';
+      };
   };
-
-  programs.nano =
-    let
-      nanoConf = lib.strings.fileContents "${self}/config/nano/nanorc";
-    in
-    {
-      enable = true;
-      nanorc = ''
-        include ${pkgs.nanorc}/share/*.nanorc # Extended syntax highlighting
-
-        ${nanoConf}
-      '';
-    };
 }
