8814e93803015115f534113baff9c4810e2d501f
Loïc Reynier <contact@loicreynier.fr>
2025-11-24 15:30:54 +0100

    refactor(config.neovim): add back `ltex_extra`

--- config/neovim/lua/plugins/lazy/lsp.lua
+++ config/neovim/lua/plugins/lazy/lsp.lua
@@ -239,4 +239,34 @@ return {
       },
     },
   },
+
+  -- External file support for LTeX
+  {
+    "barreiroleo/ltex_extra.nvim",
+    branch = "dev",
+    ft = { "markdown", "tex" },
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = function()
+      local path_
+      local git_root = vim.fs.dirname(vim.fs.find(".git", { path = vim.loop.cwd(), upward = true })[1])
+
+      if git_root then
+        if #vim.fn.glob(git_root .. "/.vscode" .. "/ltex.*", true, true) == 0 then
+          path_ = git_root .. "/.ltex"
+        else
+          path_ = git_root .. "/.vscode"
+        end
+      else
+        path_ = vim.fn.stdpath("data") .. "/ltex"
+      end
+
+      ---@module "ltex_extra"
+      ---@type LtexExtraOpts
+      ---@diagnostic disable-next-line: missing-fields
+      return {
+        load_langs = { "en", "en-US", "fr" },
+        path = path_,
+      }
+    end,
+  },
 }
