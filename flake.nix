55f3c79172abfe8a4ed036a37ee18c3a5bbb0284
Loïc Reynier <contact@loicreynier.fr>
2026-01-13 10:02:06 +0100

    build: add pre-commit, mise config and Justfile

--- flake.nix
+++ flake.nix
@@ -13,29 +13,52 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        gitHooks = self.checks.${system}.pre-commit;
       in
       {
         devShells.default = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          inherit (gitHooks) shellHook;
+          buildInputs = gitHooks.enabledPackages;
         };
 
-        checks = {
-          pre-commit-check = git-hooks.lib.${system}.run {
-            src = "./.";
-            excludes = [ "flake\.lock" ];
-            hooks = {
-              editorconfig-checker.enable = true;
-              shfmt = {
-                enable = true;
-                settings = {
-                  simplify = false;
+        checks =
+          let
+            mkHook =
+              name: args:
+              {
+                inherit name;
+                enable = args.enable or true;
+              }
+              // builtins.removeAttrs args [ "enable" ];
+          in
+          {
+            pre-commit = git-hooks.lib.${system}.run {
+              src = "./.";
+              configPath = "./.pre-commit-config-nix.yaml";
+              package = pkgs.prek;
+              excludes = [ "flake\.lock" ];
+              hooks = {
+                nixfmt = mkHook "Format >> Nix files (nixfmt)" { };
+                shfmt = mkHook "Format >> Shell files (shfmt-system)" {
+                  settings = {
+                    simplify = false;
+                  };
                 };
+                ruff-format = mkHook "Format >> Python files (ruff-format)" { };
+                end-of-file-fixer = mkHook "Format >> End of line fixer (end-of-file-fixer)" { };
+                check-symlinks = mkHook "Check  >> Symlinks (check-symlinks)" { };
+                check-added-large-files = mkHook "Check  >> Added large files (check-added-large-files)" { };
+                check-toml = mkHook "Check  >> TOML files (check-toml)" { };
+                check-yaml = mkHook "Check  >> YAML Files (check-YAML)" { };
+                typos = mkHook "Check  >> Typos (typos)" { };
+                editorconfig-checker = mkHook "Check  >> EditorConfig compliance (editorconfig-checker)" { };
+                deadnix = mkHook "Lint   >>> Nix files (deadnix)" { };
+                statix = mkHook "Lint   >>> Nix files (statix)" { };
+                ruff = mkHook "Lint   >> Python files (ruff-check)" { };
+                shellcheck = mkHook "Lint   >> Shell files (shellcheck-system)" { };
               };
-              typos.enable = true;
-              ruff.enable = true;
             };
           };
-        };
       }
     ));
 
--- scripts/pyversion.py
+++ scripts/pyversion.py
@@ -4,4 +4,3 @@ import sys
 
 for module in sys.argv[1:]:
     print(f"{module}: {__import__(module).__version__}")
-
