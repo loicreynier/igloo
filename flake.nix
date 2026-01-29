f59ac1ead774d3fce68b7711dcb1b1e14ed10e97
Loïc Reynier <contact@loicreynier.fr>
2026-01-29 09:53:49 +0100

    build(nix): switch to `flake-parts`

--- flake.nix
+++ flake.nix
@@ -2,69 +2,16 @@
   description = "Igloo";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      git-hooks,
-      ...
-    }:
-    (flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        gitHooks = self.checks.${system}.pre-commit;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit (gitHooks) shellHook;
-          buildInputs = gitHooks.enabledPackages;
-        };
-
-        checks =
-          let
-            mkHook =
-              name: args:
-              {
-                inherit name;
-                enable = args.enable or true;
-              }
-              // builtins.removeAttrs args [ "enable" ];
-          in
-          {
-            pre-commit = git-hooks.lib.${system}.run {
-              src = "./.";
-              configPath = "./.pre-commit-config-nix.yaml";
-              package = pkgs.prek;
-              excludes = [ "flake\.lock" ];
-              hooks = {
-                nixfmt = mkHook "Format >> Nix files (nixfmt)" { };
-                shfmt = mkHook "Format >> Shell files (shfmt-system)" {
-                  settings = {
-                    simplify = false;
-                  };
-                };
-                ruff-format = mkHook "Format >> Python files (ruff-format)" { };
-                end-of-file-fixer = mkHook "Format >> End of line fixer (end-of-file-fixer)" { };
-                check-symlinks = mkHook "Check  >> Symlinks (check-symlinks)" { };
-                check-added-large-files = mkHook "Check  >> Added large files (check-added-large-files)" { };
-                check-toml = mkHook "Check  >> TOML files (check-toml)" { };
-                check-yaml = mkHook "Check  >> YAML Files (check-YAML)" { };
-                typos = mkHook "Check  >> Typos (typos)" { };
-                editorconfig-checker = mkHook "Check  >> EditorConfig compliance (editorconfig-checker)" { };
-                deadnix = mkHook "Lint   >>> Nix files (deadnix)" { };
-                statix = mkHook "Lint   >>> Nix files (statix)" { };
-                ruff = mkHook "Lint   >> Python files (ruff-check)" { };
-                shellcheck = mkHook "Lint   >> Shell files (shellcheck-system)" { };
-              };
-            };
-          };
-      }
-    ));
+    { flake-parts, import-tree, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (import-tree ./nix);
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    import-tree.url = "github:vic/import-tree";
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ nix/modules/flake/pre-commit.nix
@@ -0,0 +1,56 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.git-hooks.flakeModule
+  ];
+
+  perSystem =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      mkHook =
+        name: args:
+        {
+          inherit name;
+          enable = args.enable or true;
+        }
+        // builtins.removeAttrs args [ "enable" ];
+    in
+
+    {
+      pre-commit.settings = {
+        configPath = "./.pre-commit-config-nix.yaml";
+        package = pkgs.prek;
+        excludes = [ "flake\.lock" ];
+
+        hooks = {
+          nixfmt = mkHook "Format >> Nix files (nixfmt)" { };
+          shfmt = mkHook "Format >> Shell files (shfmt-system)" {
+            settings = {
+              simplify = false;
+            };
+          };
+          ruff-format = mkHook "Format >> Python files (ruff-format)" { };
+          end-of-file-fixer = mkHook "Format >> End of line fixer (end-of-file-fixer)" { };
+          check-symlinks = mkHook "Check  >> Symlinks (check-symlinks)" { };
+          check-added-large-files = mkHook "Check  >> Added large files (check-added-large-files)" { };
+          check-toml = mkHook "Check  >> TOML files (check-toml)" { };
+          check-yaml = mkHook "Check  >> YAML Files (check-YAML)" { };
+          typos = mkHook "Check  >> Typos (typos)" { };
+          editorconfig-checker = mkHook "Check  >> EditorConfig compliance (editorconfig-checker)" { };
+          deadnix = mkHook "Lint   >>> Nix files (deadnix)" { };
+          statix = mkHook "Lint   >>> Nix files (statix)" { };
+          ruff = mkHook "Lint   >> Python files (ruff-check)" { };
+          shellcheck = mkHook "Lint   >> Shell files (shellcheck-system)" { };
+        };
+      };
+
+      devShells.default = pkgs.mkShell {
+        inherit (config.pre-commit) shellHook;
+        packages = config.pre-commit.settings.enabledPackages;
+      };
+    };
+}
